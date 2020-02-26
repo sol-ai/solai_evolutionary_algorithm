85a635cba6a5786c9b867c5658f7a739bf1d8786
Harald Vinje <haraldvinje@gmail.com>
2020-02-26 16:44:47 +0100

    added DB. Working locally, but need to get Docker build to work

--- solai_evolutionary_algorithm/database/database.py
+++ solai_evolutionary_algorithm/database/database.py
@@ -1,3 +1,25 @@
+import pymongo
+
+
 class Database:
-    #TODO: class for writing character configurations to the database
-    pass
+
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client["mydatabase"]
+    col = db["friends"]
+
+    def __init__(self):
+        my_friends = [
+            {"name": "Tristan"},
+            {"name": "Hilmo"},
+            {"name": "Gard"},
+            {"name": "Håkon"},
+            {"name": "Peder"}
+        ]
+
+        self.col.delete_many({})
+        self.col.insert_many(my_friends)
+        for friend in self.col.find():
+            print(friend)
+
+
+Database()
