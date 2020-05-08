18e5a515087b02793da949704178cb9f293de133
Eirik Skjærseth <eirik1502@gmail.com>
2020-05-08 13:56:00 +0200

    redesign property mutations

--- solai_evolutionary_algorithm/evolve_configurations/test_config.py
+++ solai_evolutionary_algorithm/evolve_configurations/test_config.py
@@ -5,35 +5,41 @@ from solai_evolutionary_algorithm.evolution.evolver import EvolverConfig, FixedG
 from solai_evolutionary_algorithm.evolution.generation_evolver import DefaultGenerationEvolver
 from solai_evolutionary_algorithm.evolution_services.plot_generations_service import PlotGenerationsLocalService
 from solai_evolutionary_algorithm.initial_population_producers.random_bounded_producer import RandomBoundedProducer
+from solai_evolutionary_algorithm.mutations.default_properties_mutation import default_properties_mutation
+from solai_evolutionary_algorithm.mutations.properties_mutation import PropertiesMutation, PropertyMutationData
+
+
 
 
 test_config = EvolverConfig(
     initial_population_producer=RandomBoundedProducer(RandomBoundedProducer.Config(
-        population_size=10,
+        population_size=100,
         character_properties_ranges={},
         melee_ability_ranges={},
         projectile_ability_ranges={}
     )),
-    # fitness_evaluator=RandomFitnessEvaluation(),
-    fitness_evaluator=SimulationFitnessEvaluation(
-        metrics=[
-            "leadChange",
-            "characterWon",
-            "stageCoverage",
-            "nearDeathFrames",
-            "gameLength"
-        ],
-        queue_host="localhost"
-    ),
+    fitness_evaluator=RandomFitnessEvaluation(),
+    # fitness_evaluator=SimulationFitnessEvaluation(
+    #     metrics=[
+    #         "leadChange",
+    #         "characterWon",
+    #         "stageCoverage",
+    #         "nearDeathFrames",
+    #         "gameLength"
+    #     ],
+    #     queue_host="localhost"
+    # ),
     # population_evolver=DefaultGenerationEvolver(DefaultGenerationEvolver.PassThroughConfig),
     population_evolver=DefaultGenerationEvolver(DefaultGenerationEvolver.Config(
         crossover_share=0.6,
         elitism_share=0.4,
         new_individuals_share=0,
         crossover=AbilitySwapCrossover(),
-        mutations=[],
+        mutations=[
+            default_properties_mutation()
+        ],
         new_individuals_producer=None
     )),
-    end_criteria=FixedGenerationsEndCriteria(generations=100),
+    end_criteria=FixedGenerationsEndCriteria(generations=10),
     generation_listeners=[PlotGenerationsLocalService()]
 )
---
+++ solai_evolutionary_algorithm/mutations/default_properties_mutation.py
@@ -0,0 +1,124 @@
+from solai_evolutionary_algorithm.mutations.properties_mutation import PropertiesMutation, PropertyMutationData
+
+
+def default_properties_mutation() -> PropertiesMutation:
+    return PropertiesMutation(
+        character_property_data={
+            'radius': PropertyMutationData(
+                float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(32, 64)
+            ),
+            'moveVelocity': PropertyMutationData(
+                float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(100, 1200)
+            )
+        },
+        melee_property_data={
+            "radius": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(5, 100)
+            ),
+            "distanceFromChar": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(0, 200)
+            ),
+            "speed": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(0, 0)
+            ),
+            "startupTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "activeTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "executionTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "endlagTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "rechargeTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(0, 60)
+            ),
+            "damage": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(10, 1000)
+            ),
+            "baseKnockback": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(10, 1000)
+            ),
+            "knockbackRatio": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(0.1, 1.0)
+            ),
+            "knockbackPoint": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(-500, 500)
+            ),
+            "knockbackTowardPoint": PropertyMutationData(
+                property_type=bool, probability=0.9,
+                mutation_factor_range=(0.5, 1.5), value_range=(False, True)
+            )
+        },
+        projectile_property_data={
+            "radius": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(5, 100)
+            ),
+            "distanceFromChar": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(0, 200)
+            ),
+            "speed": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(100, 800)
+            ),
+            "startupTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "activeTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "executionTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "endlagTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(1, 60)
+            ),
+            "rechargeTime": PropertyMutationData(
+                property_type=int, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(0, 60)
+            ),
+            "damage": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(10, 500)
+            ),
+            "baseKnockback": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(10, 1000)
+            ),
+            "knockbackRatio": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(0.1, 1.0)
+            ),
+            "knockbackPoint": PropertyMutationData(
+                property_type=float, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(-500, 500)
+            ),
+            "knockbackTowardPoint": PropertyMutationData(
+                property_type=bool, probability=0.1,
+                mutation_factor_range=(0.5, 1.5), value_range=(False, True)
+            )
+        }
+    )
