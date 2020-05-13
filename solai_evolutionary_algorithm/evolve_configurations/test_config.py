3b93eddf184a017e325fadd786cc0a26f26e5e75
Eirik Skjærseth <eirik1502@gmail.com>
2020-05-13 11:21:43 +0200

    change property ranges from list to tuple

--- solai_evolutionary_algorithm/evolve_configurations/test_config.py
+++ solai_evolutionary_algorithm/evolve_configurations/test_config.py
@@ -1,22 +1,14 @@
-from solai_evolutionary_algorithm.plot_services.plot_generations_service import PlotGenerationsLocalService
-from solai_evolutionary_algorithm.mutations.default_properties_mutation import default_properties_mutation
-from solai_evolutionary_algorithm.evolution_end_criteria.fixed_generation_end_criteria import \
-    FixedGenerationsEndCriteria
-import json
-from pkg_resources import resource_stream
-from solai_evolutionary_algorithm.initial_population_producers.random_bounded_producer import RandomBoundedProducer
-from solai_evolutionary_algorithm.evolution.fitness_and_novelty_evolver import FitnessAndNoveltyEvolver
 from solai_evolutionary_algorithm.crossovers.ability_swap_crossover import AbilitySwapCrossover
+from solai_evolutionary_algorithm.evolution.fitness_and_novelty_evolver import FitnessAndNoveltyEvolver
 from solai_evolutionary_algorithm.database.update_database_service import UpdateDatabaseService
 from solai_evolutionary_algorithm.evaluation.simulation.simulation_fitness_evaluation import SimulationFitnessEvaluation
 from solai_evolutionary_algorithm.evolution.evolver_config import EvolverConfig
-from solai_evolutionary_algorithm.evolution.generation_evolver import DefaultGenerationEvolver
+from solai_evolutionary_algorithm.evolution.fitness_and_novelty_evolver import FitnessAndNoveltyEvolver
 from solai_evolutionary_algorithm.evolution_end_criteria.fixed_generation_end_criteria import \
     FixedGenerationsEndCriteria
 from solai_evolutionary_algorithm.initial_population_producers.from_existing_producers import FromExistingProducer
 from solai_evolutionary_algorithm.initial_population_producers.random_bounded_producer import RandomBoundedProducer
 from solai_evolutionary_algorithm.mutations.default_properties_mutation import default_properties_mutation
-from solai_evolutionary_algorithm.plot_services.plot_generations_service import PlotGenerationsLocalService
 
 random_population_producer = RandomBoundedProducer(RandomBoundedProducer.Config(
     population_size=20,
@@ -25,43 +17,43 @@ random_population_producer = RandomBoundedProducer(RandomBoundedProducer.Config(
     projectile_ability_ranges={}
 ))
 
-character_properties_ranges = {"radius": [28.0, 50.0],
-                               "moveVelocity": [200.0, 2000.0]}
+character_properties_ranges = {"radius": (28.0, 50.0),
+                               "moveVelocity": (200.0, 2000.0)}
 
 melee_ability_ranges = {
     "name": "abilityName",
     "type": "MELEE",
-    "radius": [16, 200],
-    "distanceFromChar": [0, 200],
-    "speed": [0, 0],
-    "startupTime": [10, 500],
-    "activeTime": [10, 60],
-    "executionTime": [0, 60],
-    "endlagTime": [10, 60],
-    "rechargeTime": [0, 60],
-    "damage": [100, 1000],
-    "baseKnockback": [10, 1000],
-    "knockbackRatio": [0.1, 1.0],
-    "knockbackPoint": [-500, 500],
-    "knockbackTowardPoint": [False, True]
+    "radius": (16, 200),
+    "distanceFromChar": (0, 200),
+    "speed": (0, 0),
+    "startupTime": (10, 500),
+    "activeTime": (10, 60),
+    "executionTime": (0, 60),
+    "endlagTime": (10, 60),
+    "rechargeTime": (0, 60),
+    "damage": (100, 1000),
+    "baseKnockback": (10, 1000),
+    "knockbackRatio": (0.1, 1.0),
+    "knockbackPoint": (-500, 500),
+    "knockbackTowardPoint": (False, True)
 }
 
 projectile_ability_ranges = {
     "name": "abilityName",
     "type": "PROJECTILE",
-    "radius": [5, 50],
-    "distanceFromChar": [0, 200],
-    "speed": [100, 800],
-    "startupTime": [1, 60],
-    "activeTime": [20, 1000],
-    "executionTime": [0, 60],
-    "endlagTime": [6, 60],
-    "rechargeTime": [13, 80],
-    "damage": [15, 500],
-    "baseKnockback": [50, 1000],
-    "knockbackRatio": [0.1, 1.0],
-    "knockbackPoint": [-500, 500],
-    "knockbackTowardPoint": [False, True]
+    "radius": (5, 50),
+    "distanceFromChar": (0, 200),
+    "speed": (100, 800),
+    "startupTime": (1, 60),
+    "activeTime": (20, 1000),
+    "executionTime": (0, 60),
+    "endlagTime": (6, 60),
+    "rechargeTime": (13, 80),
+    "damage": (15, 500),
+    "baseKnockback": (50, 1000),
+    "knockbackRatio": (0.1, 1.0),
+    "knockbackPoint": (-500, 500),
+    "knockbackTowardPoint": (False, True)
 }
 
 from_existing_population_producer = FromExistingProducer(
@@ -107,7 +99,7 @@ test_config = EvolverConfig(
                 probability_per_bool_property=0.05,
             )
         ],
-        new_individuals_producer=[]
+        new_individuals_producer=None
     )),
     end_criteria=FixedGenerationsEndCriteria(generations=10),
     evolver_listeners=[
