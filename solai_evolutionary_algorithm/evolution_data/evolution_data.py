ec34bafe43477595b89d481e460d4b03459d1a10
Harald Vinje <haraldvinje@gmail.com>
2020-06-18 15:50:44 +0200

    new statistics retriever added

--- solai_evolutionary_algorithm/evolution_data/evolution_data.py
+++ solai_evolutionary_algorithm/evolution_data/evolution_data.py
@@ -5,7 +5,8 @@ from solai_evolutionary_algorithm.utils.character_distance_utils import create_c
 from solai_evolutionary_algorithm.evolve_configurations.sol_properties_ranges import character_properties_ranges, melee_ability_ranges, projectile_ability_ranges
 from itertools import combinations
 import random
-from statistics import mean
+from statistics import mean, variance
+from math import sqrt
 
 from typing import List
 import pymongo
@@ -49,10 +50,6 @@ class ReadOnlyDatabase:
                                              {'generations'})
 
 
-def filter_feasible_from_population(population):
-    return list(filter(lambda individual: individual['feasibility_score'] == 1, population))
-
-
 normalized_euclidean_distance = create_character_distance_func(
     character_properties_ranges=character_properties_ranges,
     melee_ability_ranges=melee_ability_ranges,
@@ -63,65 +60,47 @@ def get_last_generation_of_evolution_instance(evolution_instance) -> List:
     return evolution_instance['generations'][-1]
 
 
-def get_feasible_diversity_of_evolution_instances(evolution_instances) -> List:
-    diversities = []
-    for evolution in evolution_instances:
-        last_generation = get_last_generation_of_evolution_instance(evolution)
-        most_diverse_feasible_population = larges_permutation_diversity(
-            last_generation)
-        print(most_diverse_feasible_population)
-        diversity_of_generation = diversity(most_diverse_feasible_population)
-        diversities.append(diversity_of_generation)
-    return diversities
+def get_last_feasible_generation_of_evolution_instance(evolution_instance) -> List:
+    return filter_feasible_from_population(evolution_instance['generations'][-1])
 
 
-def avg_diversity_of_populations(populations) -> float:
-    return mean([diversity(population) for population in populations.values()])
-
-
-def average_largest_population_diversity(populations, method):
-    population_distances = [
-        larges_permutation_diversity(population)
-        for population in populations
-    ]
-    average_distance = mean(map(lambda x: x[1], population_distances))
-    max_population_with_distance = max(
-        population_distances, key=lambda x: x[1])
-
-    for char in max_population_with_distance[0]:
-        print(char['individual']['characterId'])
-
-    print(method)
-
-
-def larges_permutation_diversity(population) -> float:
-    populations_of_6 = list(combinations(population, 6))
-    permutation_distances = [
-        (sub_population, diversity(sub_population))
-        for sub_population in populations_of_6
-    ]
-
-    return max(permutation_distances, key=lambda pop: pop[1])
+def filter_feasible_from_population(population):
+    return list(filter(lambda individual: individual['feasibility_score'] == 1, population))
 
 
 def diversity(population):
     return sum([normalized_euclidean_distance(
         char1['individual'],
         char2['individual'])
-        for (char1, char2) in combinations(population, 2)])
+        for (char1, char2) in combinations(population, 2)])/len(population)
 
 
-def get_stats_from_evolution_instances(instance, method: str):
-    last_generation_feasible_individuals = []
-    print("stats")
-    for (i, e) in enumerate(instance):
-        feasible_population = filter_feasible_from_population(
-            e['generations'][-1])
-        last_generation_feasible_individuals.append(feasible_population)
-        print(
-            f"feasible individuals in last generation of method {method}: \n {len(feasible_population)}")
-    print()
-    return last_generation_feasible_individuals
+def get_diversity_of_evolution_instance(evolution_instance):
+    last_feasible_population = get_last_feasible_generation_of_evolution_instance(
+        evolution_instance)
+    return diversity(last_feasible_population)
+
+
+def get_number_of_feasible_of_evolution_instance(evolution_instance):
+    last_feasible_population = get_last_feasible_generation_of_evolution_instance(
+        evolution_instance)
+    return len(last_feasible_population)
+
+
+def print_statistics_of_evolution(evolution_instances, number_of_evolutions):
+    evolution_instances_clone = evolution_instances.clone()
+    diversities = [get_diversity_of_evolution_instance(
+        evolution_instance) for evolution_instance in evolution_instances][:number_of_evolutions]
+    no_of_feasible = [get_number_of_feasible_of_evolution_instance(
+        evolution_instance) for evolution_instance in evolution_instances_clone][:number_of_evolutions]
+    print(
+        f"Average diversity of final populations: {mean(diversities)}")
+    print(
+        f"Standard deviation diversity of final populations: {sqrt(variance(diversities))}")
+    print(
+        f"Average number of feasible individuals of final populations: {mean(no_of_feasible)}")
+    print(
+        f"Standard deviation of feasible individuals of final populations: {sqrt(variance(no_of_feasible))}\n")
 
 
 print("connecting to DB...")
@@ -133,30 +112,17 @@ FINS_w_random_evolutions = read_only_db.get_FINS_with_random_init_pop_instances(
 FINS_w_existing_evolutions = read_only_db.get_FINS_with_existing_init_pop_instances()
 FI2NS_w_random_evolutions = read_only_db.get_FI2NS_with_random_init_pop_instances()
 FI2NS_w_existing_evolutions = read_only_db.get_FI2NS_with_existing_init_pop_instances()
-print("evolutions received")
-
-get_stats_from_evolution_instances(FINS_w_random_evolutions, "FINS w random")
-get_stats_from_evolution_instances(
-    FINS_w_existing_evolutions, "FINS w existing")
-get_stats_from_evolution_instances(FI2NS_w_random_evolutions, "FI2NS w random")
-get_stats_from_evolution_instances(
-    FI2NS_w_existing_evolutions, "FI2NS w existing")
-
-FINS_w_random_diversity = get_feasible_diversity_of_evolution_instances(
-    FINS_w_random_evolutions)
-
-FINS_w_existing_diversity = get_feasible_diversity_of_evolution_instances(
-    FINS_w_existing_evolutions)
-
-FI2NS_w_random_diversity = get_feasible_diversity_of_evolution_instances(
-    FI2NS_w_random_evolutions)
-
-FI2NS_w_existing_diversity = get_feasible_diversity_of_evolution_instances(
-    FI2NS_w_existing_evolutions)
+print("evolutions received\n")
 
 
-print(f"Average diversty of different methods:\n \
-        FINS with random init pop: {mean(FINS_w_random_diversity)}\n \
-        FINS with existing init pop: {mean(FINS_w_existing_diversity)}\n\
-        FI2NS with random init pop: {mean(FI2NS_w_random_diversity)}\n\
-        FI2NS with existing init pop: {mean(FI2NS_w_existing_diversity)}\n")
+print("FINS with random initial population:")
+print_statistics_of_evolution(FINS_w_random_evolutions, 20)
+
+print("FINS with existing initial population:")
+print_statistics_of_evolution(FINS_w_existing_evolutions, 20)
+
+print("FI2NS with random initial population:")
+print_statistics_of_evolution(FI2NS_w_random_evolutions, 20)
+
+print("FI2NS with existing initial population:")
+print_statistics_of_evolution(FI2NS_w_existing_evolutions, 20)
